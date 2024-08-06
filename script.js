706e8bdec6e740dcf00a98a475295d712c78ed6b
Jelle Van Loock <22350389+jellevanloock@users.noreply.github.com>
2024-08-06 11:20:03 +0200

    - Add JS functionality: open, close modal

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 `use strict`;
+
+const modal = document.querySelector(`.modal`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsShowModal = document.querySelectorAll(`.show-modal`);
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener(`click`, function () {
+    modal.classList.remove(`hidden`);
+  });
+}
+
+btnCloseModal.addEventListener(`click`, function () {
+  modal.classList.add(`hidden`);
+});
